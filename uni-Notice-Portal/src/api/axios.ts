734e2ead8a4178748b9dc742347fc6618579f190
Samita Timsina <samitatimsina910@gmail.com>
2026-07-14 12:40:27 +0545

    precise fixes

--- uni-Notice-Portal/src/api/axios.ts
+++ uni-Notice-Portal/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth } from "firebase/auth";
 
 export const api = axios.create({
-  baseURL: "https://university-notice-portal.onrender.com/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 api.interceptors.request.use(async (config) => {
