2918d36c5b40da36ad8e6d43f6438a4c02f09bb1
Samita Timsina <samitatimsina910@gmail.com>
2026-07-14 14:01:13 +0545

    profile upload issue fixed

--- backend/src/middlewares/uploadProfile.ts
+++ backend/src/middlewares/uploadProfile.ts
@@ -2,10 +2,17 @@ import type { FileFilterCallback } from "multer";
 import multer from "multer";
 import path from "path";
 import type { Request } from "express";
+import fs from "fs";
+
+const uploadDir = path.join(process.cwd(), "uploads", "profiles");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/profiles");
+    cb(null, uploadDir);
   },
 
   filename: (req, file, cb) => {
