4c067389a1b0a09969d65b382bc5e285219f15f7
Samita Timsina <samitatimsina910@gmail.com>
2026-07-14 12:07:55 +0545

    Bug fixes

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,9 +1,12 @@
 import { initializeApp, cert } from "firebase-admin/app";
-import serviceAccount from "../../firebase-service-account.json" with { type:"json"};
 import type { ServiceAccount } from "firebase-admin/app";
 
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+) as ServiceAccount;
+
 const app = initializeApp({
-  credential: cert(serviceAccount as ServiceAccount),
+  credential: cert(serviceAccount),
 });
 
 export default app;
